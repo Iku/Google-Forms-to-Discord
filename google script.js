e6701c8fb53eceb7855e066ef0bd189db331e835
untuned <untunedmc@gmail.com>
2018-12-02 13:56:14 -0500

    formatting

--- google script.js
+++ google script.js
@@ -1,4 +1,4 @@
-﻿var POST_URL = "WEBHOOKURL";
+var POST_URL = "WEBHOOKURL";
 
 function onSubmit(e) {
   var form = FormApp.getActiveForm();
@@ -6,35 +6,44 @@ function onSubmit(e) {
   var latestResponse = allResponses[allResponses.length - 1];
   var response = latestResponse.getItemResponses();
   var items = [];
-  
-  for (var i = 0; i < response.length ; i++) {
+
+  for (var i = 0; i < response.length; i++) {
     var question = response[i].getItem().getTitle();
     var answer = response[i].getResponse();
     var parts = answer.match(/[\s\S]{1,1024}/g) || [];
-    
-    if (answer == "") {continue;}
-    for (var j = 0; j< parts.length; j++) {
-      if (j == 0) {
-        items.push({"name": question, "value": parts[j], "inline": false});
-      } else {
-      items.push({"name": question.concat(" (cont.)"), "value": parts[j], "inline": false});
-      }
-    }   
-  }
-  
-  var options = {
-    "method" : "post",
-    "payload": JSON.stringify({
-      "embeds": [
-        {
-          "title":"TOP TEXT CHANGE THIS IN SCRIPT",
-          "fields":items,
-          "footer":{"text": "BOTTOM TEXT CHANGE THIS IN SCRIPT"}
-        }
-      ]
+
+    if (answer == "") {
+      continue;
     }
-   )
+    for (var j = 0; j < parts.length; j++) {
+      if (j == 0) {
+        items.push({
+          "name": question,
+          "value": parts[j],
+          "inline": false
+        });
+      } else {
+        items.push({
+          "name": question.concat(" (cont.)"),
+          "value": parts[j],
+          "inline": false
+        });
+      }
+    }
+  }
+
+  var options = {
+    "method": "post",
+    "payload": JSON.stringify({
+      "embeds": [{
+        "title": "TOP TEXT CHANGE THIS IN SCRIPT",
+        "fields": items,
+        "footer": {
+          "text": "BOTTOM TEXT CHANGE THIS IN SCRIPT"
+        }
+      }]
+    })
   };
 
-   UrlFetchApp.fetch(POST_URL, options);
+  UrlFetchApp.fetch(POST_URL, options);
 };
